a1578c8b7066485e67dfc8609591d6de15f66ab0
Anand Vishwakarma <rd.anandv@gmail.com>
2023-04-08 15:24:41 +0530

    user authentication & register api created

---
+++ AuthenticationAPI/Context/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using AuthenticationAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationAPI.Context
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+    
+}
